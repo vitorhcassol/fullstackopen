fc6f258dc6b5949260dd4d554218311248f40f59
Vitor Cassol <80921175+vitorhcassol@users.noreply.github.com>
2023-08-06 18:20:37 -0400

    ex1.7 finished

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -11,17 +11,40 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  const [positive, setPositive] = useState(0)
+  const [average, setAverage] = useState(0)
 
   const addGood = () => {
-    setGood(good + 1)
+    const atualizeGood = good + 1
+    const atualizeAll = all + 1
+    setGood(atualizeGood)
+    addAll()
+    setAverage((atualizeGood - bad) / atualizeAll)
+    setPositive(atualizeGood / atualizeAll)
   }
 
   const addNeutral = () => {
-    setNeutral(neutral + 1)
+    const atualizeNeutral = neutral + 1
+    const atualizeAll = all + 1
+    setNeutral(atualizeNeutral)
+    addAll()
+    setAverage((good - bad) / atualizeAll)
+    setPositive(good / atualizeAll)
   }
 
   const addBad = () => {
-    setBad(bad + 1)
+    const atualizeBad = bad + 1
+    const atualizeAll = all + 1
+    setBad(atualizeBad)
+    addAll()
+    setAverage((good - atualizeBad) / atualizeAll)
+    setPositive(good / atualizeAll)
+  }
+
+  const addAll = () => {
+    const atualizeAll = all + 1
+    setAll(atualizeAll)
   }
 
   return (
@@ -34,6 +57,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positive} %</p>
     </div>
   )
 }
