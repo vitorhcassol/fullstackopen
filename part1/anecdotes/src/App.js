ddb3dd60bc9e497263ada3e91f7302a58c6fa716
Vitor Cassol <80921175+vitorhcassol@users.noreply.github.com>
2023-08-06 22:11:17 -0400

    ex1.14 finished

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -14,21 +14,30 @@ const App = () => {
 
   const [selected, setSelected] = useState(0)
   const [votes, setVotes] = useState(new Uint8Array(8))
+  const [maxValue, setMaxValue] = useState(0)
+  const [mostVoted, setMostVoted] = useState(0)
 
   const nextAnecdote = () => setSelected(Math.floor(Math.random() * 7))
 
   const addVote = () => {
-    const copy = { ...votes }
+    const copy = [ ...votes ]
     copy[selected] += 1
     setVotes(copy)
+    const max = copy.reduce((prev, current) => prev > current ? prev : current);
+    setMaxValue(max)
+    setMostVoted(copy.indexOf(max))
   }
 
   return (
     <div>
+      <h1>Anecdote of the day</h1>
       <p>{anecdotes[selected]}</p>
       <p>has {votes[selected]} votes</p>
       <button onClick={addVote}>vote</button>
       <button onClick={nextAnecdote}>next anecdote</button>
+      <h1>Anecdote with most votes</h1>
+      <p> {anecdotes[mostVoted]} </p>
+      <p> has {maxValue} votes</p>
     </div>
   )
 }
